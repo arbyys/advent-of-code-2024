c2b343c5bf95694aa45ce01c14e642db9bc25f5a
arbyys <adam.petricek@centrum.cz>
2024-12-01 17:24:06 +0100

    Day1, part1

--- src/days/01/part1.ts
+++ src/days/01/part1.ts
@@ -1,4 +1,49 @@
-import { Effect } from "effect";
+import { Effect, pipe, Schema, Either, Array, Order } from "effect";
 
-export const part1 = (): Effect.Effect<string, never> =>
-    Effect.succeed("Result of Part 1");
+const LineSchema = Schema.Array(Schema.NumberFromString);
+
+const parse = (data: string) => {
+    const lines = data.trim().split("\n");
+
+    const col1: number[] = [];
+    const col2: number[] = [];
+
+    lines.map((line) => {
+        const parts = line.trim().split(/\s+/);
+        const decode = Schema.decodeUnknownEither(LineSchema);
+
+        const result = decode(parts);
+        if(Either.isRight(result)) {
+            col1.push(result.right[0]);
+            col2.push(result.right[1]);
+        } else
+            Effect.fail("Syncing failed");
+    });
+
+    return [col1, col2];
+}
+
+const sortArrays = (array: number[][]): number[][] => {
+    return [
+        [...array[0]].sort(),
+        [...array[1]].sort()
+    ]
+}
+
+const reduceDistances = (array: number[][]): number => {
+    return array[0].reduce((acc, value, index) => {
+        return acc + Math.abs(value - array[1][index]);
+    }, 0);
+    
+}
+
+export const part1 = (data: string): Effect.Effect<number, never, never> => {
+    const parsedData = parse(data);
+    const process = pipe(
+        parsedData,
+        sortArrays,
+        reduceDistances,
+        Effect.succeed,
+    );
+    return process;
+}
--- src/runner.ts
+++ src/runner.ts
@@ -1,4 +1,6 @@
 import { Effect, pipe } from "effect";
+import * as path from "path";
+import * as fs from "fs/promises";
 
 const zeroPad = (num: any, places: number) => String(num).padStart(places, '0');
 
@@ -6,8 +8,11 @@ const logHeader = (day: string, part: string) => {
     console.log(`\nDay ${day}, ${part}:`);
 };
 
+const readFile = (filePath: string) => 
+    Effect.promise(() => fs.readFile(filePath, "utf-8"));
+
 const run = async () => {
-    const [day, part] = process.argv.slice(2);
+    const [day, part, sample] = process.argv.slice(2);
 
     if (!day || !part) {
         console.error("Usage: tsx runner.ts <day> <part1/part2>");
@@ -26,21 +31,27 @@ const run = async () => {
         process.exit(1);
     }
 
-    await pipe(
-        partFunction(),
-        Effect.runPromise
+    const sampleText = !!sample ? '-sample' : '';
+
+    const filePath = path.join(__dirname, `./data/${dayPadded}${sampleText}.txt`);
+
+    const effect = pipe(
+        readFile(filePath),
+        Effect.andThen((content) => partFunction(content))
     )
+
+    Effect.runPromise(effect as Effect.Effect<string, never, never>)
         .then((result) => {
             logHeader(dayPadded, part);
             const endTime = Date.now();
-            const duration = (endTime - startTime);
+            const duration = endTime - startTime;
             console.log(`Result - ${result}`);
             console.log(`\n(Execution time: ${duration}ms)\n`);
         })
         .catch((err) => {
             logHeader(dayPadded, part);
             console.error(`[!] Error: ${err}\n`);
-        })
+        });
 };
 
 run();
