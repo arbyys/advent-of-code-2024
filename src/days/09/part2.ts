b81bdb75ef19f2173bb2ad32d634c91feefe1a47
arbyys <adam.petricek@centrum.cz>
2024-12-11 15:06:49 +0100

    Day 09 files

---
+++ src/days/09/part2.ts
@@ -0,0 +1,28 @@
+import { Effect, pipe, Schema } from "effect";
+
+const parse = (data: string) => {
+    const lines = data.trim().split("\n");
+
+    /*const col1: number[] = [];
+
+    lines.map((line) => {
+        const parts = line.trim().split(/\s+/);
+        const decode = Schema.decodeUnknownEither(LineSchema);
+
+        const result = decode(parts);
+        if(Either.isRight(result)) {
+            col1.push(result.right[0]);
+        } else
+            Effect.fail("Syncing failed");
+    });
+
+    */
+    return lines;
+}
+
+export const part2 = (data: string): Effect.Effect<unknown, never, never> => {
+    return pipe(
+        parse(data),
+        Effect.succeed,
+    )
+}
