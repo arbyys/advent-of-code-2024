6071b112f635d7b5ceec07442f6d85934571790d
arbyys <adam.petricek@centrum.cz>
2024-12-11 18:15:52 +0100

    Day 09 both parts

--- src/days/09/part1.ts
+++ src/days/09/part1.ts
@@ -1,26 +1,109 @@
-import { Effect, pipe, Schema } from "effect";
+import { Effect, pipe, Schema, Either } from "effect";
 
 const parse = (data: string) => {
-    const lines = data.trim().split("\n");
+    const lines = data.trim().split("");
 
-    const col1: number[] = [];
+    let disks: number[] = [];
 
     lines.map((line) => {
-        const parts = line.trim().split(/\s+/);
-        const decode = Schema.decodeUnknownEither(LineSchema);
+        const decode = Schema.decodeUnknownEither(Schema.NumberFromString);
 
-        const result = decode(parts);
-        if(Either.isRight(result)) {
-            col1.push(result.right[0]);
+        const result = decode(line);
+        if (Either.isRight(result)) {
+            disks.push(result.right);
         } else
             Effect.fail("Syncing failed");
     });
-    return lines;
+    return disks;
+}
+
+class DiskMap extends Array<number | null> {
+    constructor(arr: (number | null)[]) {
+        super();
+        this.push(...arr);
+    }
+
+    fillFirstEmpty(id: number): boolean {
+        for (let i = 0; i < this.length; i++) {
+            if (this[i] === null) {
+                this[i] = id;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    popLastId(): number | null {
+        let last;
+        do {
+            last = this.pop();
+        } while (last === null);
+        return !!last ? last : null;
+    }
+
+    hasSomeEmpty(): boolean {
+        return this.some(item => item === null);
+    }
+
+    getItems(): (number | null)[] {
+        return [...this];
+    }
+}
+
+const printData = (data: (number | null)[]): void => {
+    console.log('\n' + data.map(item => item === null ? "." : item ).join(""));
+}
+
+const constructDiskMapArray = (dataRaw: number[]): (number | null)[] => {
+    let currentId = 0;
+    const result: (number | null)[] = [];
+
+    dataRaw.forEach((currentNumber, idx) => {
+        const fillValue = idx % 2 === 0 ? currentId : null;
+        if (idx % 2 === 0) currentId++;
+        for (let i = 0; i < currentNumber; i++) {
+            result.push(fillValue);
+        }
+    });
+
+    //console.log("r:", result);
+    return result;
+}
+
+const calculateChecksum = (data: number[]): bigint => {
+    return data.reduce((checksum: bigint, id: number, idx) => {
+        if (idx%20==0)console.log(checksum, BigInt(id) * BigInt(idx));
+        return checksum + BigInt(id) * BigInt(idx);
+    }, BigInt(0));
+}
+
+const process = (data: number[]): number[] => {
+    const diskMapArray = constructDiskMapArray(data);
+
+    const diskMap = new DiskMap(diskMapArray);
+    console.log("Start");
+
+    let iterNum = 0;
+
+    while(true) {
+        //console.log("Current iter:", iterNum, diskMap.getItems());
+        const id = diskMap.popLastId();
+        if (!id) break;
+        if (!!id && !diskMap.hasSomeEmpty()) { diskMap.push(id); break; }
+        diskMap.fillFirstEmpty(id);
+        iterNum += 1;
+    }
+
+    console.log("END", diskMap.getItems())
+
+    return diskMap.getItems() as number[];
 }
 
 export const part1 = (data: string): Effect.Effect<unknown, never, never> => {
     return pipe(
         parse(data),
+        process,
+        calculateChecksum,
         Effect.succeed,
     );
 }
--- src/days/09/part2.ts
+++ src/days/09/part2.ts
@@ -1,28 +1,144 @@
-import { Effect, pipe, Schema } from "effect";
+import { Effect, pipe, Schema, Either } from "effect";
+
+interface DataPacked {
+    array: (number | null)[],
+    lastId: number
+}
 
 const parse = (data: string) => {
-    const lines = data.trim().split("\n");
+    const lines = data.trim().split("");
 
-    /*const col1: number[] = [];
+    let disks: number[] = [];
 
     lines.map((line) => {
-        const parts = line.trim().split(/\s+/);
-        const decode = Schema.decodeUnknownEither(LineSchema);
+        const decode = Schema.decodeUnknownEither(Schema.NumberFromString);
 
-        const result = decode(parts);
-        if(Either.isRight(result)) {
-            col1.push(result.right[0]);
+        const result = decode(line);
+        if (Either.isRight(result)) {
+            disks.push(result.right);
         } else
             Effect.fail("Syncing failed");
     });
+    return disks;
+}
 
-    */
-    return lines;
+class DiskMap extends Array<number | null> {
+    constructor(arr: (number | null)[]) {
+        super();
+        this.push(...arr);
+    }
+
+    tryInsertFile(id: number, originalIndex: number, length: number): boolean {
+        let count = 0;
+        let foundIndex = -1;
+
+        for (let i = 0; i < this.length; i++) {
+            if(i>originalIndex) break;
+            if (this[i] === null) {
+                count++;
+                if (count === length) {
+                    foundIndex = i - length + 1;
+                    break;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        
+        if (foundIndex == -1) return false;
+
+        for (let i = foundIndex; i < foundIndex + length; i++) {
+            this[i] = id;
+        }
+        return true;
+    }
+
+    getFileLengthAndIndex(id: number): {length: number | undefined, indexes: number[]} {
+        let count = 0;
+        let indexes = [];
+
+        for (let i = 0; i < this.length; i++) {
+            if (this[i] === id) {
+                count++;
+                indexes.push(i);
+            }
+        }
+
+        return {length: (count > 0 ? count : undefined), indexes: indexes};
+    }
+
+    hasSomeEmpty(): boolean {
+        return this.some(item => item === null);
+    }
+
+    getItems(): (number | null)[] {
+        return [...this];
+    }
+}
+
+const printData = (data: (number | null)[]): void => {
+    console.log(data.map(item => item === null ? "." : item).join(""));
+}
+
+const constructDiskMapArray = (dataRaw: number[]): DataPacked => {
+    let currentId = 0;
+    const result: (number | null)[] = [];
+
+    dataRaw.forEach((currentNumber, idx) => {
+        const fillValue = idx % 2 === 0 ? currentId : null;
+        if (idx % 2 === 0) currentId++;
+        for (let i = 0; i < currentNumber; i++) {
+            result.push(fillValue);
+        }
+    });
+
+    //console.log("r:", result);
+    return { array: result, lastId: currentId };
+}
+
+const calculateChecksum = (data: (number | null)[]): bigint => {
+    return data.reduce((checksum: bigint, id: number | null, idx) => {
+        if (id === null) return checksum;
+        //if (idx % 20 == 0) console.log(checksum, BigInt(id) * BigInt(idx));
+        return checksum + BigInt(id) * BigInt(idx);
+    }, BigInt(0));
+}
+
+const process = (data: number[]): (number | null)[] => {
+    const { array, lastId } = constructDiskMapArray(data);
+
+    const diskMap = new DiskMap(array);
+    console.log("Start");
+    //printData(array);
+
+    let currentFileToMove = lastId-1;
+
+    while (true) {
+        if (currentFileToMove < 0) break;
+        const {length, indexes} = diskMap.getFileLengthAndIndex(currentFileToMove);
+        if (!length) {
+            currentFileToMove--;
+            continue;
+        };
+
+        const res = diskMap.tryInsertFile(currentFileToMove, indexes[0], length);
+
+        if(res) indexes.map(i => diskMap[i] = null);
+
+        currentFileToMove--;
+    }
+
+    console.log("END")
+    //printData(diskMap.getItems());
+
+    return diskMap.getItems() as (number | null)[];
 }
 
 export const part2 = (data: string): Effect.Effect<unknown, never, never> => {
     return pipe(
         parse(data),
+        process,
+        calculateChecksum,
         Effect.succeed,
-    )
+    );
 }
